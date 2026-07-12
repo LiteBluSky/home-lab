f3701bda139e2829832699d0eee534fcc205c92e
LiteBluSky <rbuckbamford@gmail.com>
2026-07-12 18:07:52 +0200

    nix: updated imports

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./filebrowser.nix
     ./copyparty.nix
+    ./wol-python/wol.nix
     # ./isolated-browser.nix
     # ./desktop-app-3.nix  <-- Want to disable an app? Just comment it out here!
   ];
