a00e4a6dc463fb48ac57bc0e30d55e8abce50a5c
LiteBluSky <rbuckbamford@gmail.com>
2026-07-16 23:24:59 +0200

    services: added couch db for obsidian notes

---
+++ modules/services/couchdb.nix
@@ -0,0 +1,12 @@
+{
+  # 1. Enable CouchDB
+  services.couchdb = {
+    enable = true;
+    package = pkgs.couchdb3;
+    # CouchDB binds to localhost by default; change to "0.0.0.0" to expose it to your home network
+    bindAddress = "0.0.0.0"; 
+  };
+
+  # 2. Open the port in your firewall (CouchDB default port is 5984)
+  networking.firewall.allowedTCPPorts = [ 5984 ];
+}
--- modules/services/default.nix
+++ modules/services/default.nix
@@ -5,6 +5,7 @@
     ./filebrowser.nix
     ./copyparty.nix
     ./wol-python/wol.nix
+    ./couchdb.nix
     # ./isolated-browser.nix
     # ./desktop-app-3.nix  <-- Want to disable an app? Just comment it out here!
   ];
